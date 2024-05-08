ca5da82f8f7aaa93864cf58bf795f37b6d22c218
Vikash Rai <raivikash174@gmail.com>
2024-05-08 09:33:20 +0530

    variable understanding

---
+++ variables.ts
@@ -0,0 +1,17 @@
+// number
+
+let userID = 234325;
+
+userID.toFixed();
+
+//boolean
+let isLoggedIn: boolean = false;
+
+//any
+let hero;
+
+function getHero() {
+  return "IronMan";
+}
+
+hero = getHero();
